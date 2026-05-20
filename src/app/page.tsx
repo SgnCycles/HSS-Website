81ffabfac65a98acdfe2ccb5a9d4ec8a71d4c071
SgnCycles <signe.bekere@gmail.com>
2026-05-20 17:39:28 +0200

    feat(018): add age group card (Home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <AgeGroupCardHome />
+        <AgeGroupCardHome/>
       </main>
     </div>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,15 @@
 export type ageGroupCardContentType = {
-    title: string,
-    ageGroup: string,
-    ageRange: string,
-    subtitle: string,
-    info: string,
-    meetings: string,
-    colourSchemePrimary: string,
-    colourSchemeSecondary: string,
-    icon: string,
-    image: string,
-    price: string,
-    apply: string
+  title: string,
+  ageGroup: string,
+  ageRange: string,
+  subtitle: string,
+  info: string,
+  meetings: string,
+  colourSchemeBorder: string,
+  colourSchemePrimary: string,
+  colourSchemeSecondary: string,
+  icon: string,
+  image: string,
+  price: string,
+  apply: string
 }
