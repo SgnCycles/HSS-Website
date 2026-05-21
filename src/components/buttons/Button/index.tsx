2159b459c20ffc38b40dc89f4cf3e682a8b4921d
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-05-21 16:16:24 +0200

    feat(016): primary and types added

---
+++ src/components/buttons/Button/index.tsx
@@ -0,0 +1,20 @@
+import { ButtonProps } from "@/types/common.types"
+import Link from "next/link"
+
+
+export default function Button({ ariaLabel, href, theme, text }: ButtonProps) {
+  const themeClasses = {
+    primaryBrown: "font-bold bg-brown-900 hover:bg-brown-900/70 text-white border-accent hover:border-accent/50",
+    secondary: "sm:ps-[50px] sm:pe-[50px] font-bold inline-6/7 bg-primary hover:bg-primary-hover text-accent hover:text-accent-hover border-accent hover:border-accent/50",
+    primaryBlue: "font-semibold bg-primary hover:bg-primary-hover text-accent hover:text-accent-hover border-accent hover:border-accent/50",
+  }
+  return ( 
+    <Link 
+      className={` ${themeClasses[theme]} text-center px-[64px] w-full sm:w-max transition-all duration-300 border-2 inline-flex items-center justify-center rounded-sm py-2 min-h-[70px]`}
+      href={href}
+      aria-label={ariaLabel}
+    >
+      {text}
+    </Link>
+  )
+} 
--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -1,3 +1,13 @@
 export type CommonType = {
   className: string,
 }
+
+export type ButtonProps = {
+  ariaLabel: string, 
+  href: string,
+  theme: 
+    | 'primaryBrown'
+    | 'primaryBlue'
+    | 'secondary'
+  text: string,
+}
