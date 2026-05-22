afc7f8f7db7fda61ce3e783b0bd560f56bae6d52
SgnCycles <signe.bekere@gmail.com>
2026-05-22 15:27:16 +0200

    fix(023): merged into dev

--- src/components/cards/BliLedare/index.tsx
+++ src/components/cards/BliLedare/index.tsx
@@ -8,7 +8,7 @@ const BliLedareHome = () => {
   <article className="bg-primary font-body text-background p-6 md:grid md:place-items-center">
     <div className="md:grid md:place-items-center w-full">
       <div className="flex w-full justify-center items-center">
-        <Image src={`/icons/party-leader-icon.svg`} height={30} width={30} alt=""/>
+        <Image src={`/Icons/party-leader-icon.svg`} height={30} width={30} alt=""/>
         <h2 className="font-heading text-[25px] md:text-[40px] font-extrabold pl-2 md:text-5xl"><span className="text-accent">Bli ledare - </span>vi utbildar dig!</h2>
       </div>
       <p className="text-muted text-base md:text-xl pt-4 pb-4">
@@ -20,7 +20,7 @@ const BliLedareHome = () => {
           {bliLedareCardContent && bliLedareCardContent.map((card, index) => (
             <li key={index} className="flex border border-muted rounded-md pt-4 pb-4 bg-[#1A496F]">
               <div className="w-[30%] md:w-[20%] grid place-items-center">
-                <Image src={`/icons/${card.icon}`} height={30} width={30} alt="" />
+                <Image src={`/Icons/${card.icon}`} height={30} width={30} alt="" />
               </div>
               <div>
                 <h3 className="font-heading text-lg font-semibold">{card.title}</h3>
@@ -34,7 +34,7 @@ const BliLedareHome = () => {
           <div className="hidden md:block">
             <Image src={'/images/Body/home_body2.png'} height={300} width={300} alt=""/>
           </div>
-          <a className="w-full border border-accent bg-primary-hover rounded-sm p-4 text-lg cursor-pointer" href="https://www.scoutnet.se/register/in/group/764" target="_blank">Anmäll ditt intresse <span className="pl-1">&#8594;</span></a>
+          <a className="w-full border border-accent bg-primary-hover text-accent rounded-sm p-4 text-lg cursor-pointer" href="https://www.scoutnet.se/register/in/group/764" target="_blank">Anmäll ditt intresse <span className="pl-1">&#8594;</span></a>
           <a className="border border-accent rounded-sm p-4 w-[70%] text-base text-accent cursor-pointer" href="/bli-ledare">Mer information här</a>
         </div>
       </div>
