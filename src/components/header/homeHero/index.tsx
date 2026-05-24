24ca0a60be1f86c5c9e965be062797e775cef22a
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-24 12:10:22 +0200

    fix: move desktop hero title higher on homepage

--- src/components/header/homeHero/index.tsx
+++ src/components/header/homeHero/index.tsx
@@ -15,8 +15,8 @@ export default function HomeHero({ img }: HeroProps) {
         backgroundPosition: "left bottom",
       }}
     >
-      <div className="hidden md:flex flex-col text-center relative">
-        <h1 className="font-heading font-extrabold text-[64px] leading-none text-center">
+      <div className="hidden md:flex flex-col text-center relative -translate-y-65">
+        <h1 className="font-body font-extrabold text-[64px] leading-none text-center">
           <span className="block text-white">Välkommen till </span>
           <span className="block text-accent">Hässelby Strand Sjöscoutkår</span>
         </h1>
