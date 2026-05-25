a0d436e5461175f4849ba87e5bf764506be34c0c
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-25 14:38:58 +0200

    feat(006)-reusable responsive hero for Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import AgeGroupCardHome from "@/components/cards/AgeGroupCardHome";
-
+import HomeHero from "@/components/header/homeHero";
 export default function Home() {
   return (
     <div className="">
+      <HomeHero img="/images/Hero/home_hero.png" />
       <main className="">
         <AgeGroupCardHome />
       </main>
