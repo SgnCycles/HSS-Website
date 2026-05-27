5aa98ed6e2a22119c084719dde700c579a2f2228
SgnCycles <signe.bekere@gmail.com>
2026-05-27 11:37:39 +0200

    feat(012): add header and nav

---
+++ src/components/headers/HeaderHome/index.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image"
+import Link from "next/link"
+import Nav from "@/components/common/Nav"
+
+const HeaderHome = () => {
+  return (
+    <>
+      <header
+        className="h-72 w-full md:h-screen bg-cover bg-no-repeat flex flex-col items-center justify-between"
+        style={{
+          backgroundImage: "linear-gradient(to bottom, #00355F 0%, rgba(0,53,95,0.8) 30%, rgba(0,53,95,0.2) 60%, transparent 100%), url('/images/Hero/aldersgrupp_hero.png')",
+          backgroundPosition: "left bottom",
+        }}
+      >
+        <Nav />
+        <h1 className="hidden sm:block font-heading text-2xl lg:text-[64px] md:text-4xl font-extrabold leading-none text-center text-shadow-black text-shadow-xs text-white xl:pb-50">
+          Välkommen till <br />
+          <span className="text-accent">Hässelby Strand Sjöscoutkår</span>
+        </h1>
+        <div
+          className="hidden sm:2-[80%] pt-12 md:flex justify-around items-center bg-cover w-full h-64"
+          style={{
+            backgroundImage: "url('/images/Backgrounds/homewavy.svg')",
+          }}
+        >
+          <p className="font-bold font-body text-background mt-10">
+            {/* removed unnecessary spans */}
+            Vi är en stolt sjöscoutkår som ger barn och ungdomar en trygg plats
+            <br />
+            att växa, lära sig sjömanskap och skapa livslånga vänner.
+          </p>
+          <Link
+            href="/"
+            className="cursor-pointer py-2 px-12 font-body font-bold bg-primary rounded-sm text-accent border border-[#99AEBF] mt-10"
+          >
+            Bli scout
+          </Link>
+        </div>
+        <div className="sm:hidden">
+          <Image
+            src="/images/Backgrounds/homewavy.svg"
+            alt=""
+            width={393}
+            height={55}
+            className="md:hidden w-full"
+          ></Image>
+        </div>
+      </header>
+      <section className="sm:hidden bg-primary py-6 flex flex-col items-center gap-3">
+        <h1 className="sm:hidden text-xl lg:text-[64px] font-heading font-extrabold top-47 text-center text-white">
+          Välkommen till <br/>
+          <span className="text-accent"> Hässelby Strand Sjöscoutkår</span>
+        </h1>
+        <p className="w-[90%] font-body text-sm font-medium text-background text-center left-4">
+          Vi är en sjöscoutkår där barn och ungdomar får växa, lära sig
+          sjömanskap och skapa vänner för livet.
+        </p>
+        <Link
+          href="/vad-gor-vi"
+          className="w-[90%] h-12 mt-2 cursor-pointer pt-5 pb-5 pr-16 pl-16 font-body font-semibold bg-primary rounded-sm text-accent border-accent border flex justify-center items-center"
+        >
+          Bli scout
+        </Link>
+      </section>
+    </>
+  )
+}
+
+export default HeaderHome
