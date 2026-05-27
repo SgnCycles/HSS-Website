8cafc4f9157aac353aa4bb3e82150ec169fac5db
Clara Swahn <clara.swahn@gmail.com>
2026-05-27 13:16:02 +0200

    feat(048): add pdf file and mobile version of page

--- src/app/(om-oss)/vilka-ar-vi/page.tsx
+++ src/app/(om-oss)/vilka-ar-vi/page.tsx
@@ -5,14 +5,14 @@ const VilkaArViPage = () => {
   return (
     <>
       <article className="flex flex-col items-center">
-        <div className="flex flex-row items-center pb-2.5 gap-2.5">
+        <div className="flex flex-row items-center p-2.5 gap-2.5">
           <Image src={`/images/Logo/logo.png`} height={30} width={30} alt="logo" className="h-max" />
-          <h1 className="font-heading text-[25px] font-semibold text-blue-900 ">Om HSS</h1>
+          <h1 className="font-heading text-[25px] font-semibold text-blue-900">Om HSS</h1>
         </div>
-        <div className="flex gap-2 pl-2 pr-2 justify-center rounded-md w-fit border bg-secondary border-accent mb-2.5">
+        <div className="flex gap-2 pl-2 pr-2 justify-center rounded-md w-fit border bg-secondary border-accent mb-5">
           <p className="font-body">Scoutkåren bildades 1959</p>
         </div>
-        <div className="font-body p-5">
+        <div className="font-body p-5 pt-0">
           <p>Hässelby Strands Sjöscoutkår (HSS) är en traditionsrik ideell ungdomsverksamhet med fokus på scouting och båtliv.
             Vi är religiöst och politisk obundna, och medlem i Svenska Scoutförbundet, som med ca 65.000 medlemmar är en av Sveriges största ungdomsorganisationer.</p>
         </div>
@@ -22,18 +22,18 @@ const VilkaArViPage = () => {
         <p className="font-body p-5">Scoutkåren bildades 1959 och bedriver verksamheten i en trevlig lokal (kallad Ruffen) vid Mälarens strand i Hässelby. Kåren har omkring 130 glada medlemmar, äger fem segelbåtar, tio optimistjollar, två 2-kronor, snabb följebåt och en stor scoutstuga.</p>
         <Image src={`/images/Body/vilkaViAr_body3.png`} height={276} width={393} alt="vilkaViAr" />
       </article>
-      <article className="flex flex-col bg-yellow-200">
+      <article className="flex flex-col bg-yellow-200 p-5">
         <h2 className="font-heading text-[20px] font-semibold text-blue-900">Se andra dokument</h2>
-        <Link target="_blank" href="" className="font-body">Stadgar för Hässelby Strands Sjöscoutkår →</Link>
-        <Link target="_blank" href="" className="font-body">Verksamhetsplan för perioden 2025-2026 →</Link>
-        <Link target="_blank" href="" className="font-body">HSS Kår stämma för 2025 →</Link>
+        <Link target="_blank" href="/pdf/Stadgar-Hasselby-Strands-Sjoscoutkar-beslutad-2025-10-22-signerade.pdf" className="font-body underline underline-offset-2 text-blue-900">Stadgar för Hässelby Strands Sjöscoutkår →</Link>
+        <Link target="_blank" href="/pdf/Verksamhetsplan-for-perioden-2025-09-01-2026-12-31.pdf" className="font-body underline underline-offset-2 text-blue-900">Verksamhetsplan för perioden 2025-2026 →</Link>
+        <Link target="_blank" href="/pdf/HSS-karstamma-2025-10-22-protokoll-signerat" className="font-body underline underline-offset-2 text-blue-900">HSS Kår stämma för 2025 →</Link>
       </article>
       <article>
-        <div className="flex flex-row items-center">
-          <Image src={`/images/Logo/logo.png`} height={30} width={30} alt="logo" className="h-max" />
+        <div className="flex flex-row items-center p-5 gap-2.5 pb-0">
+          <Image src={`/Icons/yellow-anchor-icon.svg`} height={30} width={30} alt="anchor" className="h-max" />
           <h2 className="font-heading text-[25px] font-semibold text-blue-900">Historien om HSS</h2>
         </div>
-        <div>
+        <div className="p-5">
           <p>HSS grundades år 1959 och växte snabbt tack vare ett starkt engagemang från både ledare, föräldrar och scouter. Tillsammans hjälptes man åt att bygga upp verksamheten genom aktiviteter, insamlingar och ideellt arbete.
             <br></br><br></br>
             Kårens första båt, Igelknäcken (“Inga-Lunda”), blev starten på HSS starka sjöscouttradition. Med tiden kunde fler båtar köpas in tack vare basarer, lotterier och ett stort gemensamt engagemang.
@@ -50,7 +50,7 @@ const VilkaArViPage = () => {
           <Image src={`/images/Body/vilkaViAr_historia_body3.png`} height={276} width={393} alt="historia" />
           <Image src={`/images/Backgrounds/blueWavyLine.png`} height={29} width={350} alt="wavy-line" />
         </div>
-      </article>
+      </article >
     </>
   )
 }
