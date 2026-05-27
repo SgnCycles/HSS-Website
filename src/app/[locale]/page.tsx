549911ec4f98dfe324935d6c7cee1e8c5e018a69
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-27 13:02:59 +0200

    add

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,9 @@
 import AgeGroupCardHome from "@/components/cards/AgeGroupCardHome";
-import LanguageSwitcher from "@/components/LanguageButton";
-import { useTranslations } from "next-intl";
 
 export default function Home() {
 
-  const t = useTranslations("home");
   return (
     <div className="">
-      <LanguageSwitcher />
-      <h1>{t("title")}</h1>
-   
       <main className="">
         <AgeGroupCardHome />
       </main>
