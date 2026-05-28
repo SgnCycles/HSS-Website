c41afb56593ef6983e9d3e1d9b07310ca72592b0
Villiam Agnas <villiamagnas@gmail.com>
2026-05-28 13:24:42 +0200

    fix(014):fixed more responsivness

--- src/app/(bli-scout)/vad-gor-vi/page.tsx
+++ src/app/(bli-scout)/vad-gor-vi/page.tsx
@@ -3,7 +3,6 @@ import ActivityFeature from "@/components/cards/ActivityFeature"
 const VadGorViPage = () => {
   return (
     <>
-    <h1>Placeholder: This is "Vad gör vi" page</h1>
       <ActivityFeature />
     </>
   )
--- src/components/cards/ActivityCard/index.tsx
+++ src/components/cards/ActivityCard/index.tsx
@@ -5,12 +5,14 @@ const ActivityCard = () => {
   return(
     <ul className="flex flex-col gap-3 md:grid md:grid-cols-2">
       {activityCardContent && activityCardContent.map((card, index) => (
-        <li key={index} className="rounded-md border ml-4 mr-4 bg-secondary border-accent pr-2 pl-2 min-h-27.5 md:ml-0 md:mr-0">
-          <div className="flex gap-2 p-1 md:p-4">
+        <li key={index} className="rounded-md border ml-4 mr-4 bg-secondary border-accent pr-2 pl-2 min-h-27.5 md:pt-2 md:pb-2 md:ml-0 md:mr-0">
+          <div className="flex gap-2 p-1">
             <Image className="w-auto h-auto" src={card.icon} alt="" width={20} height={20}/>
-            <h2 className="text-base font-heading font-semibold text-primary lg:text-[22px]">{card.title}</h2>
+            <div className="md:flex md:items-center md:min-h-14">
+              <h2 className="text-base font-heading font-semibold text-primary lg:text-[22px]">{card.title}</h2>
+            </div>
           </div>
-          <p className="text-[15px] p-1 font-body sm:text-[18px] md:p-3">{card.info}</p>
+          <p className="text-[15px] p-1 font-body sm:text-[18px]">{card.info}</p>
         </li>
       ))}
     </ul>
