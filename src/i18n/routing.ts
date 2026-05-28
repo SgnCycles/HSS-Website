19a9ae89e7b061c68618d64b2207acf57ddf6cd2
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-28 23:39:47 +0200

    fix(routing): disable locale cookie and browser detection to always default to sv

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,8 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["sv", "en"],
   defaultLocale: "sv",
+  localeCookie: false,
+  localeDetection: false,
 });
 
 export const { useRouter, usePathname, Link } = createNavigation(routing);
