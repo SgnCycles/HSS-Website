8012afc2672ed13169579428ce0de4f619091fdd
Villiam Agnas <villiamagnas@gmail.com>
2026-05-28 12:31:01 +0200

    fix(021):fixed responsive design

--- src/components/cards/SecurityAndSafetyFeature/index.tsx
+++ src/components/cards/SecurityAndSafetyFeature/index.tsx
@@ -17,35 +17,37 @@ const SecurityAndSafetyFeature = () => {
   }, [popup])
 
   return (
-    <article className="flex flex-col items-center self-center mt-8 mb-8 max-w-[80%]">
-      <div className="flex flex-col items-center md:flex-row md:items-start md:gap-10 md:justify-between">
-        <section className="flex flex-col items-center md:items-start">
-          <div className="flex items-center p-4 md:pt-0">
-            <Image className="h-auto w-7" src={"/Icons/yellow-shield-cross-icon.svg"} alt="" height={40} width={40} />
-            <h1 className="font-bold font-heading text-primary text-[25px]">Säkerhet & trygghet</h1>
+    <article className="flex flex-col items-center self-center mt-8 mb-8 lg:max-w-[80%]">
+      <div className="flex flex-col items-center lg:flex-row lg:items-start lg:gap-10 lg:justify-between">
+        <section className="flex flex-col items-center lg:mb-6 lg:mt-6 lg:items-start">
+          <div className="flex items-center justify-center w-full lg:p-4 lg:pt-0">
+            <Image className="h-auto w-8" src={"/Icons/yellow-shield-cross-icon.svg"} alt="" height={40} width={40} />
+            <h1 className="font-bold font-heading ml-2 text-primary text-[25px] md:text-3xl lg:text-[40px]">Säkerhet & trygghet</h1>
           </div>
-          <p className="p-4 ml-4 font-body max-w-md md:p-0">På HSS är barnens trygghet, inkludering och glädje alltid vår högsta prioritet.</p>
+          <p className="pb-4 pt-0 font-body mt-2 w-[90%] text-center max-w-lg lg:text-left lg:text-lg lg:p-0 lg:pl-6">På HSS är barnens trygghet, inkludering och glädje alltid vår högsta prioritet.</p>
         </section>
-        <ul className="flex flex-col">
-          {securityAndSafetyCheckContent && securityAndSafetyCheckContent.map((box, index) => (
-            <li className="flex items-start gap-2 p-1" key={index}>
-              <Image className="h-auto w-5" src={box.icon} alt="" height={40} width={40} />
-              <p className="font-body text-sm md:text-[16px]">{box.info}</p>
-            </li>
-          ))}
-        </ul>
+        <div className="mt-auto">
+          <ul className="flex flex-col mb-4 ">
+            {securityAndSafetyCheckContent && securityAndSafetyCheckContent.map((box, index) => (
+              <li className="flex items-start gap-2 p-1" key={index}>
+                <Image className="h-auto w-5" src={box.icon} alt="" height={40} width={40} />
+                <p className="font-body text-base">{box.info}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <SecurityAndSafetyCard />
-      <div>
+      <div className="flex flex-col justify-center items-center">
         {securityAndSafetyInfoContent && securityAndSafetyInfoContent.map((box, index) => (
-          <div className="p-4 md:hidden" key={index}>
-            <h2 className="font-heading font-medium text-[20px] text-primary">{box.title}</h2>
+          <div className="pb-2 pt-2 w-[90%] lg:hidden" key={index}>
+            <h2 className="font-heading font-medium pb-2 text-[20px] text-primary">{box.title}</h2>
             <p className="font-body">{box.info}</p>
           </div>
         ))}
       </div>
-      <div className="hidden w-full justify-end p-4 md:flex">
-        <button className="bg-primary p-4 text-accent rounded-sm text-[18px] font-semibold font-body cursor-pointer" aria-label="Öppna popup" onClick={() => setPopup(true)}>Läs mer här</button>
+      <div className="hidden w-full justify-end p-4 lg:flex">
+        <button className="bg-primary p-4 text-accent rounded-sm text-[18px] font-bold font-body cursor-pointer" aria-label="Öppna popup" onClick={() => setPopup(true)}>Läs mer här</button>
       </div>
       {popup && (
         <div className="fixed w-full h-full z-50 top-0 left-0 flex items-center justify-center">
@@ -61,7 +63,7 @@ const SecurityAndSafetyFeature = () => {
               </section>
             ))}
             <div className="flex justify-end">
-              <button className="mt-4 bg-primary p-4 text-accent rounded-sm text-[18px] font-semibold font-body cursor-pointer" aria-label="Stäng popup" onClick={() => setPopup(false)}>Stäng</button>
+              <button className="mt-4 bg-primary p-4 text-accent rounded-sm text-[18px] font-bold font-body cursor-pointer" aria-label="Stäng popup" onClick={() => setPopup(false)}>Stäng</button>
             </div>
           </article>
         </div>
