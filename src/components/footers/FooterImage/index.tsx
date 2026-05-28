bd27ef0f393ab9280add849372ec96577f3977cf
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-05-28 09:16:28 +0200

    refractor(055): implemented semantic HTML structure on footer + change in FooterImage

--- src/components/footers/FooterImage/index.tsx
+++ src/components/footers/FooterImage/index.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 
 const FooterImage = () => {
-  return  <Image 
-            src="/images/Footer/footer.png" 
-            height={527} 
-            width={1722} 
-            alt="Illustration av Hässelby Sjörscoutkårs klubbhus" 
-            className="w-full h-auto" 
-            priority={false}
-          />
+  return (
+    <Image 
+      src="/images/Footer/footer.png" 
+      height={527} 
+      width={1722} 
+      alt="Illustration av Hässelby Sjörscoutkårs klubbhus" 
+      className="w-full h-auto" 
+      priority={false}
+    />
+  )
 }
 
 export default FooterImage
