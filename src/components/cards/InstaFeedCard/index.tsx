2b81324e432335af699fbdf85091a9cd16ee1bc2
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-29 14:11:53 +0200

    fix

--- src/components/cards/InstaFeedCard/index.tsx
+++ src/components/cards/InstaFeedCard/index.tsx
@@ -15,7 +15,7 @@ export default function InstaFeedCard() {
 
   const [apiData, setApiData] = useState<FeedProps[] >([])
   const [like, setLike] = useState(false)
-  const [likeCount, setLikeCount] = useState(null)
+  
 
   const url = "https://feeds.behold.so/4FiCVYtO0Wj9jbmn9bBs"
 
@@ -45,11 +45,7 @@ export default function InstaFeedCard() {
    
   }
 
-  const handelLikeCount=()=>{
-
-
-    setLikeCount(likeCount+1)
-  }
+  
 
   return (
 
@@ -59,8 +55,8 @@ export default function InstaFeedCard() {
           <div key={post.id} className="shadow-2xl shadow-black rounded-sm">
            
             <Image src={post.mediaUrl} alt="" width={300} height={400} className="mt-6"/>
-            <div className="flex justify-start mt-3"><Heart className="mr-4 cursor-pointer" onClick={handelLike} fill={like? "#ef4444" : "none"} stroke={like? "#ef4444" : "currentColor"}/><p className="">{likeCount}</p> <MessageCircle className="" /> <div><Share className=""/></div></div>
-            <p className="mt-2">{likeCount}Likes</p>
+            <div className="flex justify-start mt-3"><Heart className="mr-4 cursor-pointer" onClick={handelLike} fill={like? "#ef4444" : "none"} stroke={like? "#ef4444" : "currentColor"}/><MessageCircle className="" /> <div><Share className=""/></div></div>
+            <p className="mt-2">Likes</p>
             <div className="flex justify-between items-center mt-4"><p className="">{post.caption}</p><button className="text-grey-500 font-medium">See translation</button></div>
           </div>
         )
