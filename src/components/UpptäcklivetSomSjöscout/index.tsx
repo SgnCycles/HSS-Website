3a6662d46b6c1cacef2a5acdd7c637e8a02204f9
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-31 21:13:06 +0200

    fix image responsiveness

--- src/components/UpptäcklivetSomSjöscout/index.tsx
+++ src/components/UpptäcklivetSomSjöscout/index.tsx
@@ -4,83 +4,75 @@ import Link from "next/link";
 
 export default function UpptäckLivetSomSjöscout() {
   return (
-
     <>
-    <InstaFeedCard />
-    <section className="w-full relative flex flex-col justify-center gap-6 items-center pb-25 md:pt-30 px-4">
-
-    
-      <Image
-        src="/Icons/fe_arrow-down.svg"
-        alt=""
-        width={59}
-        height={59}
-        className="hidden md:block absolute top-10 mx-auto"
-      />
-      <div className="flex flex-col items-start w-full md:max-w-6xl  mx-auto  gap-6">
-        <h2 className="flex gap-2 pt-4">
-          <Image
-            src="/Icons/yellow-instagram-icon.svg"
-            alt=""
-            width={41}
-            height={41}
-            className="w-10 h-10 md:w-10.25 md:h-10.25"
-          />
-          <span className="text-primary md:text-4xl text-xl font-heading font-extrabold">
-            Upptäck livet som sjöscout
-          </span>
-        </h2>
-        <div className="flex flex-col md:flex-row items-center gap-6">
-          <Link
-            href="https://www.instagram.com/hss_scout/"
-            className="flex gap-3 text-center justify-center items-center bg-primary px-6 py-3 rounded-sm"
-          >
-            <Image
-              src="/Icons/skill-icons_instagram.svg"
-              alt=""
-              width={72}
-              height={72}
-              className="w-18 h-18"
-            />
-            <div className="flex flex-col">
-              <span className="block font-body text-accent font-extrabold">
-                Följ oss på instagram{" "}
-              </span>
-              <span className="font-body text-background font-bold ">
-                @hss_scout
-              </span>
+      <section className="w-full relative flex flex-col justify-center gap-6 items-center pb-25 md:pt-30 px-4">
+        <Image
+          src="/Icons/fe_arrow-down.svg"
+          alt=""
+          width={59}
+          height={59}
+          className="hidden md:block absolute top-10 mx-auto"
+        />
+        <div className="-bg-conic-60z-50">
+          <div className="flex flex-col items-start w-full md:max-w-6xl  mx-auto  gap-6">
+            <h2 className="flex items-center gap-2 pt-4 text-primary md:text-4xl text-xl font-heading font-extrabold">
+              <Image
+                src="/Icons/yellow-instagram-icon.svg"
+                alt=""
+                width={41}
+                height={41}
+                className="w-10 h-10 md:w-10.25 md:h-10.25"
+              />
+                Upptäck livet som sjöscout
+            </h2>
+            <div className="flex flex-col md:flex-row items-center gap-6">
+              <Link
+                href="https://www.instagram.com/hss_scout/" rel="noopener noreferrer"
+                className="flex gap-3 text-center justify-center items-center bg-primary px-6 py-3 rounded-sm"
+              >
+                <Image
+                  src="/Icons/skill-icons_instagram.svg"
+                  alt=""
+                  width={72}
+                  height={72}
+                  className="w-18 h-18"
+                />
+                <div className="flex flex-col">
+                  <span className="block font-body text-accent font-extrabold">
+                    Följ oss på instagram{" "}
+                  </span>
+                  <span className="font-body text-background font-bold ">
+                    @hss_scout
+                  </span>
+                </div>
+                <Image
+                  src="/Icons/yellowArrow.svg"
+                  alt=""
+                  width={33}
+                  height={33}
+                  className="transotion-transform w-8.25 h-8.25 animate-pulse duration-100"
+                />
+              </Link>
+              <div className="text-black font-medium flex flex-col">
+                <span>
+                  Kliv in i vår vardag, läger, segling, knutar och kamratskap.
+                </span>
+                <span>
+                  Se våra äventyr, vänskap och minnen som vi skapar tillsammans.
+                </span>
+              </div>
             </div>
-            <Image
-              src="/Icons/yellowArrow.svg"
-              alt=""
-              width={33}
-              height={33}
-              className="transotion-transform w-8.25 h-8.25 animate-pulse duration-100"
-            />
-          </Link>
-          <div className="text-[#363636] font-medium flex flex-col">
-            <span>
-              Kliv in i vår vardag, läger, segling, knutar och kamratskap.
-            </span>
-            <span>
-              Se våra äventyr, vänskap och minnen som vi skapar tillsammans.
-            </span>
+            <InstaFeedCard />
           </div>
         </div>
-      
-      </div>
-      <Image
-        src="/Icons/fe_arrow-down.svg"
-        alt=""
-        width={59}
-        height={59}
-        className="hidden md:block absolute bottom-0 mx-auto"
-      />
-    </section>
-    
-    
-    
+        <Image
+          src="/Icons/fe_arrow-down.svg"
+          alt=""
+          width={59}
+          height={59}
+          className="hidden md:block absolute bottom-0 mx-auto"
+        />
+      </section>
     </>
-    
   );
 }
--- src/components/cards/InstaFeedCard/index.tsx
+++ src/components/cards/InstaFeedCard/index.tsx
@@ -1,68 +1,25 @@
-"use client"
-import Image from "next/image"
-import { useEffect, useState } from "react"
-import { Share,MessageCircle, Heart} from "lucide-react"
-
-interface FeedProps{
-  id?: string,
-  caption?: string,
-  mediaUrl:string
-}
-
-
+import Image from "next/image";
+import { instaFeed } from "@/data/upptäckLivetSomSjöscout";
 
 export default function InstaFeedCard() {
-
-  const [apiData, setApiData] = useState<FeedProps[] >([])
-  const [like, setLike] = useState(false)
-  
-
-  const url = "https://feeds.behold.so/4FiCVYtO0Wj9jbmn9bBs"
-
-  const fetchInstaData = async () => {
-
-    const res = await fetch(url)
-
-    if(!res.ok){
-
-      console.log(`Error Occured ${res.status}`)
-    }
-
-    const data = await res.json()
-
-    setApiData(data.posts)
-
-  }
-
-  useEffect(() => {
-
-    fetchInstaData()
-
-  }, [])
-
-  const handelLike=()=>{
-    setLike((prev)=> !prev)
-   
-  }
-
-  
-
   return (
-
-    <div className="flex flex-col items-center w-full h-auto">
-      {apiData  && apiData.map((post) => {
+    <div className="flex md:gap-4 gap-2">
+      {instaFeed.map((post) => {
         return (
-          <div key={post.id} className="shadow-2xl shadow-black rounded-sm">
-           
-            <Image src={post.mediaUrl} alt="" width={300} height={400} className="mt-6"/>
-            <div className="flex justify-start mt-3"><Heart className="mr-4 cursor-pointer" onClick={handelLike} fill={like? "#ef4444" : "none"} stroke={like? "#ef4444" : "currentColor"}/><MessageCircle className="" /> <div><Share className=""/></div></div>
-            <p className="mt-2">Likes</p>
-            <div className="flex justify-between items-center mt-4"><p className="">{post.caption}</p><button className="text-grey-500 font-medium">See translation</button></div>
+          <div
+            key={post.id}
+            className="relative w-27 h-57 md:w-108 md:h-140 max-w-6xl"
+          >
+            <Image
+              src={post.img}
+              alt=""
+              width={432}
+              height={560}
+              className="object-cover w-full h-27 md:h-125"
+            />
           </div>
-        )
+        );
       })}
     </div>
-
-  )
-
+  );
 }
