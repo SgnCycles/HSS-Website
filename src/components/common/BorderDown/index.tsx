b6b2b467a9898d2f2f8d2484f79a4ad8ebe88f88
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-06-01 14:04:09 +0200

    feat(068): border down component and border fix

--- src/components/common/BorderDown/index.tsx
+++ src/components/common/BorderDown/index.tsx
@@ -1,9 +1,9 @@
 import { CommonType } from "@/types/common.types"
 
-export default function Border({ variant = "" }: CommonType) {
+export default function BorderDown({ variant = "" }: CommonType) {
   return (
     <svg 
-      className={`pt-4 md:pt-6 lg:pt-8 h-auto full-bleed -mb-px text-${variant}`}
+      className={`pt-4 md:pt-6 lg:pt-8 h-auto full-bleed rotate-180 -mt-px text-${variant}`}
       xmlns="http://www.w3.org/2000/svg" 
       width="1920.785" 
       height="152" 
