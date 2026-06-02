6517e665bb9a479c4bd0d91f70dc91d808b774d2
SgnCycles <signe.bekere@gmail.com>
2026-06-02 14:28:47 +0200

    feat/036-Bli Ledare Page

--- src/components/cards/BliLedare/index.tsx
+++ src/components/cards/BliLedare/index.tsx
@@ -1,83 +1,82 @@
 import Image from "next/image";
 import { bliLedareCardContent } from "@/data/bliLedareCardContent";
+import Button from "@/components/buttons/Button";
 
 const BliLedareHome = () => {
   return (
     //TODO: a background wavy image will be applied later
-    <article className="bg-primary font-body text-background pt-6 pb-6 md:grid md:place-items-center md:pt-20 md:pb-20">
-      <div className="md:grid md:place-items-center w-full">
-        <div className="flex w-full justify-center items-center">
+    <section className="bg-primary font-body text-background grid place-items-center">
+      <article className="container">
+        <div className="flex w-full justify-center md:justify-start items-center pb-6">
           <Image
             src={`/Icons/party-leader-icon.svg`}
             height={30}
             width={30}
             alt=""
           />
-          <h2 className="font-heading text-[22px] md:text-[40px] font-extrabold pl-2 md:text-5xl text-nowrap">
+          <h2 className="font-heading text-[24px] md:text-[30px] font-extrabold pl-2 text-nowrap">
             <span className="text-accent">Bli ledare - </span>vi utbildar dig!
           </h2>
         </div>
-        <div className="flex flex-col items-center">
-          <p className="text-muted text-base md:text-xl pt-4 pb-4 w-[90%] text-center">
-            Du behöver inga förkunskaper. Vi skickar dig på Scouternas
-            ledarutbildning och du får stöd av erfarna ledare från dag ett.
-          </p>
-          <div className="flex flex-col justify-between md:flex-row w-[90%] lg:w-[70%]">
-            <ul className="flex flex-col gap-6 md:gap-4 pb-6 md:pb-0">
-              {bliLedareCardContent &&
-                bliLedareCardContent.map((card, index) => (
-                  <li
-                    key={index}
-                    className="flex border border-muted rounded-md pt-4 pb-4 bg-[#1A496F]"
-                  >
-                    <div className="w-[30%] md:w-[20%] grid place-items-center">
-                      <Image
-                        src={`/Icons/${card.icon}`}
-                        height={30}
-                        width={30}
-                        alt=""
-                      />
-                    </div>
-                    <div>
-                      <h3 className="font-heading text-lg font-semibold">
-                        {card.title}
-                      </h3>
-                      <p className="text-sm text-balance md:text-wrap">
-                        {card.subtitle}
-                      </p>
-                    </div>
-                  </li>
-                ))}
-            </ul>
-            {/* TODO: buttons will be replaced with button components later */}
-            <div className="flex flex-col justify-between items-center text-center gap-6 font-bold md:pl-8">
-              <div className="hidden md:block">
-                <Image
-                  className="rounded-md"
-                  src={"/images/Body/home_body2.png"}
-                  height={300}
-                  width={300}
-                  alt=""
-                />
-              </div>
-              <a
-                className="w-full border border-accent bg-primary-hover text-accent rounded-sm p-4 text-lg cursor-pointer"
-                href="https://www.scoutnet.se/register/in/group/764"
-                target="_blank"
-              >
-                Anmäl ditt intresse <span className="pl-1">&#8594;</span>
-              </a>
-              <a
-                className="border border-accent rounded-sm p-4 w-[70%] text-base text-accent cursor-pointer"
-                href="/bli-ledare"
-              >
-                Mer information här
-              </a>
+        <p className="paragraph-light text-muted">
+          Du behöver inga förkunskaper. Vi skickar dig på Scouternas
+          ledarutbildning och du får stöd av erfarna ledare från dag ett.
+        </p>
+        <div className="flex flex-col justify-between md:flex-row">
+          <ul className="cards-column pb-6 md:pb-0 w-full md:pr-6">
+            {bliLedareCardContent &&
+              bliLedareCardContent.map((card, index) => (
+                <li
+                  key={index}
+                  className="flex border border-muted rounded-md pt-5 pb-5 pr-5 bg-[#1A496F]"
+                >
+                  <div className="w-[30%] sm:w-[20%] grid place-items-center">
+                    <Image
+                      src={`/Icons/${card.icon}`}
+                      height={30}
+                      width={30}
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-heading text-[18px] md:text-[20px] font-semibold">
+                      {card.title}
+                    </h3>
+                    <p className="text-base text-wrap">
+                      {card.subtitle}
+                    </p>
+                  </div>
+                </li>
+              ))}
+          </ul>
+          <div className="flex flex-col justify-between items-center text-center gap-6 font-bold">
+            <div className="hidden md:flex">
+              <Image
+                className="rounded-md"
+                src={"/images/Body/home_body2.png"}
+                height={300}
+                width={300}
+                alt=""
+              />
             </div>
+            <Button
+              ariaLabel=""
+              href="https://www.scoutnet.se/register/in/group/764"
+              variant="primaryBrown"
+              text="Anmäl ditt intresse"
+              target="_blank"
+            />
+            <Button
+              ariaLabel=""
+              href="/bli-ledare"
+              variant="primaryBlue"
+              text="Mer information här"
+              target="_blank"
+            />
           </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </section>
   );
 };
 
