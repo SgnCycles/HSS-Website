1ed60b4e1737ba425f014325e738d9481856d4f4
Clara Swahn <clara.swahn@gmail.com>
2026-06-03 11:27:53 +0200

    fix(063): fix icon sizing again

--- src/components/footers/FooterContact/index.tsx
+++ src/components/footers/FooterContact/index.tsx
@@ -17,7 +17,7 @@ const FooterContact = ({ title, icon, children }: FooterContactType) => {
             src={`/Icons/${icon}`}
             height={30}
             width={30}
-            className="w-3.75 h-3.75 md:w-4 md:h-4 lg:w-6.25 lg:h-6.25"
+            className="w-3.75 h-3.75 md:w-4.5 lg:w-6.25 lg:h-6.25 md:h-auto"
             alt=""
           />
         }
