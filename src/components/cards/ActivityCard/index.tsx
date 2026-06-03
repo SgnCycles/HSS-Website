0c75b3aa6ed9f3693f79962b76fc1bff575606d6
Villiam Agnas <villiamagnas@gmail.com>
2026-06-03 10:56:46 +0200

    fix(065):fixed comments on cr

--- src/components/cards/ActivityCard/index.tsx
+++ src/components/cards/ActivityCard/index.tsx
@@ -5,11 +5,11 @@ const ActivityCard = () => {
   return(
     <ul className="flex flex-col lg:grid lg:grid-cols-2 section">
       {activityCardContent && activityCardContent.map((card, index) => (
-        <li key={index} className="rounded-md border card bg-secondary border-accent pr-2 pl-2 min-h-30.75 md:min-h-38.75 lg:pt-2 lg:pb-2 lg:ml-0 lg:mr-0">
+        <li key={index} className="border card bg-secondary border-accent ">
           <div className="flex">
             <Image className="w-auto h-auto mr-2.5" src={card.icon} alt="" width={20} height={20}/>
             <div className="lg:flex lg:items-center lg:min-h-14">
-              <h2 className="text-base font-heading font-semibold text-primary heading-3 text-[18px] md:text-[20px]">{card.title}</h2>
+              <h3 className="text-primary heading-3">{card.title}</h3>
             </div>
           </div>
           <p className="paragraph-light">{card.info}</p>
--- src/components/cards/ActivityFeature/index.tsx
+++ src/components/cards/ActivityFeature/index.tsx
@@ -17,7 +17,7 @@ const ActivityFeature = () => {
           <div className="text-center lg:flex ">
             <div className="flex gap-2 p-1 justify-center">
               <Image className="h2-icon" src={activityFeatureContent.icon} alt="" width={20} height={20} />
-              <h2 className="font-heading font-extrabold text-primary heading-2 text-[24px] md:text-[30px]">{activityFeatureContent.title}</h2>
+              <h2 className="text-primary heading-2">{activityFeatureContent.title}</h2>
             </div>
             <div className="flex gap-2 pl-2 pr-2 justify-center lg:rounded-lg lg:border lg:ml-4 lg:mt-auto lg:mb-auto lg:bg-secondary lg:border-accent ">
               <Image className="hidden mt-1 w-4 h-4 lg:block" src={activityFeatureContent.subIcon} alt="" width={20} height={20} />
@@ -29,10 +29,10 @@ const ActivityFeature = () => {
           </div>
           <p className="hidden paragraph-light lg:block">{activityFeatureContent.infoDescFirst}</p>
           <p className="hidden paragraph-light lg:block">{activityFeatureContent.infoDescSecond}</p>
-          <div className="mt-4 xl:max-h-125 lg:flex section">
+          <div className="mt-4 lg:flex section">
             <ActivityCard />
             {!isHomePage && (
-              <p className="paragraph-light lg:hidden">{activityFeatureContent.info}</p>
+              <p className="paragraph-light pt-6 lg:hidden">{activityFeatureContent.info}</p>
             )}
             <Image className="hidden h-auto w-full object-cover object-top mt-6 lg:mt-0 md:block rounded-md border border-accent" src={"/images/Body/home_body1.png"} alt="" height={1000} width={1000} />
           </div>
