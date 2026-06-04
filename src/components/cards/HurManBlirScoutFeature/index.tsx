70975222c510c4a4fcac2d61a54bcd5f0a229568
Villiam Agnas <villiamagnas@gmail.com>
2026-06-04 14:52:55 +0200

    feat(064):hur man blir scout feature

--- src/components/cards/HurManBlirScoutFeature/index.tsx
+++ src/components/cards/HurManBlirScoutFeature/index.tsx
@@ -1,15 +1,50 @@
+import Link from "next/link"
 import HurManBlirScoutSteps from "../HurManBlirScoutSteps"
+import Button from "@/components/buttons/Button"
+import Image from "next/image"
 
 const HurManBlirScoutFeature = () => {
   return(
     <div className="full-bleed bg-primary">
       <div className="container">
-        <div>
-          <h2 className="heading-2 text-white">Hur man blir scout?</h2>
+        <div className="section flex md:ml-6">
+          <Image className="h-auto w-auto mt-auto mb-auto md:h-10" src={"/icons/yellow-two-people-icon.svg"} height={50} width={50} alt="" />
+          <h2 className="heading-2 text-white">Hur man blir <span className="text-accent">scout?</span></h2>
         </div>
         <p className="paragraph-light text-white">Det är enkelt att komma igång! Fyra steg och du är en del av vår kårfamilj.</p>
+        <div className="md:flex">
+          <div className="section">
+            <HurManBlirScoutSteps />
+          </div>
+          <div className="section gap-4 flex flex-col items-center">
+            <div className="card border border-accent flex flex-col justify-center bg-[#00000033] items-center text-white mt-4">
+              <ul className="border-b">
+                <div className="flex">
+                  <h3 className="heading-3 text-white">Terminavgift inkluderar</h3>
+                </div>
+                <div className="flex">
+                  <Image className="h-auto w-auto mb-4 mr-1.5 md:h-5" src={"/icons/yellow-check-icon.svg"} height={50} width={50} alt="" />
+                  <li className="paragraph-light">Veckomöten & aktiviteter</li>
+                </div>
+                <div className="flex">
+                  <Image className="h-auto w-auto mb-4 mr-1.5 md:h-5" src={"/icons/yellow-check-icon.svg"} height={50} width={50} alt="" />
+                  <li className="paragraph-light">Försäkring via Scouterna</li>
+                </div>
+                <div className="flex">
+                  <Image className="h-auto w-auto mb-4 mr-1.5 md:h-5" src={"/icons/yellow-check-icon.svg"} height={50} width={50} alt="" />
+                  <li className="paragraph-light">Tillgång till båtar & utrustning</li>
+                </div>
+              </ul>
+              <div className="flex gap-2">
+                <p>Har du frågor?</p>
+                <Link className="underline" href="/vanliga-fragor">Besök FAQ</Link>
+              </div>
+            </div>
+            <Button ariaLabel="link" variant="primaryBrown" href="/aldersgrupper" text="Läs mer om åldersgrupper" />
+            <Link href={"https://www.scoutnet.se/register/in/group/764"} target="_blank" className="font-bold bg-primary hover:bg-primary-hover text-accent hover:text-accent-hover border-accent hover:border-accent/50 text-center px-[42px] w-[80%] sm:w-max transition-all duration-300 border inline-flex items-center justify-center rounded-sm py-2 min-h-[70px]">Ansök om att bli scout! →</Link>
+          </div>
+        </div>
       </div>
-      <HurManBlirScoutSteps />
     </div>
   )
 }
--- src/components/cards/HurManBlirScoutSteps/index.tsx
+++ src/components/cards/HurManBlirScoutSteps/index.tsx
@@ -4,7 +4,7 @@ const HurManBlirScoutSteps = () => {
   return (
     <div>
       {BlirScoutStepsContent && BlirScoutStepsContent.map((entry, index) => (
-        <div key={index} className="grid grid-cols-4 grid-rows-2 gap-2 m-2">
+        <ul key={index} className="grid grid-cols-4 grid-rows-2 gap-2 m-2">
           <div className="flex justify-center mt-auto">
             <div className="bg-primary z-20 border-accent border-3 text-accent rounded-[50%] w-12.5 h-12.5 text-[26px] flex justify-center items-center">{entry.step}</div>
           </div>
@@ -13,9 +13,9 @@ const HurManBlirScoutSteps = () => {
           </div>
           <div className="col-span-3 row-span-2 col-start-2 row-start-1">
             <h3 className="text-[18px] font-heading font-semibold text-white">{entry.title}</h3>
-            <p className="text-[14px] mt-2 font-body paragraph-light text-white">{entry.info}</p>
+            <li className="text-[14px] mt-2 font-body paragraph-light text-white">{entry.info}</li>
           </div>
-        </div>
+        </ul>
       ))}
 
     </div>
