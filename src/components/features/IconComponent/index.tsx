a4b1107298f4897f1c8d0faeb5523aaa64d1af5c
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-06-04 12:37:39 +0200

    feat(074): IconsComponent

---
+++ src/components/features/IconComponent/index.tsx
@@ -0,0 +1,47 @@
+import { FaAnchor, FaCompass, FaShieldAlt, FaHandHoldingHeart, FaAward, FaHome, FaHandsHelping, FaInstagram, FaArrowRight, FaArrowLeft, FaGraduationCap, FaHeart } from "react-icons/fa";
+import { FaFire, FaSailboat } from "react-icons/fa6";
+import { IoBookOutline, IoCloseOutline, IoHandLeft, IoHelpBuoy, IoHelpCircle, IoInformationCircle, IoKey, IoMap, IoMenuOutline, IoPeople, IoToday } from "react-icons/io5";
+
+const iconList = {
+  compass: FaCompass,
+  anchor: FaAnchor,
+  buoy: IoHelpBuoy,
+  key: IoKey,
+  map: IoMap,
+  shield: FaShieldAlt,
+  heartHands: FaHandHoldingHeart,
+  info: IoInformationCircle,
+  question: IoHelpCircle,
+  parent: IoHandLeft,
+  badge: FaAward,
+  boat: FaSailboat,
+  house: FaHome,
+  policy: IoBookOutline,
+  group: IoPeople,
+  leader: FaHandsHelping,
+  fire: FaFire,
+  instagram: FaInstagram,
+  arrowRight: FaArrowRight,
+  arrowLeft: FaArrowLeft,
+  graduationCap: FaGraduationCap,
+  heart: FaHeart,
+  hamburgerMenu: IoMenuOutline,
+  closeMenu: IoCloseOutline,
+  calendar: IoToday,
+} as const;
+
+export type IconsProps = {
+  name: keyof typeof iconList | string,
+  size?: number | string,
+  variant?: string 
+}
+
+export default function Icons({ name, size = 24, variant = "text-primary" }: IconsProps) {
+  const IconComponent = iconList[name as keyof typeof iconList] || FaCompass
+
+  return (
+    <figure className="inline-grid place-items-center">
+      <IconComponent size={size} className={variant} />
+    </figure>
+  )
+}
