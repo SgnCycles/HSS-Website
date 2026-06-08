ec9c1d4c4ece4e702f34056025339d1e9edf44a2
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-06-08 21:58:30 +0200

    fix styling and add gdpr functionality

--- src/app/[locale]/(om-oss)/policy/page.tsx
+++ src/app/[locale]/(om-oss)/policy/page.tsx
@@ -1,46 +1,61 @@
+"use client"
 import Button from "@/components/buttons/Button";
 import Image from "next/image";
 import { registerInfo } from "@/data/registerInformation";
 import { collectedInformation } from "@/data/collectedInformation";
+import GDPR from "@/components/gdpr";
+import { useState } from "react";
+
 const PolicyPage = () => {
+
+  const[gdpr , setGdpr] = useState<boolean>(false);
+
+  const handelGdpr=()=>{
+
+    setGdpr((prev) => !prev)
+  }
   return (
     <main>
-      <section>
-        <article className="container">
-          <div className="flex">
+      <section className="">
+        <article className="container flex flex-col gap-15">
+          <div className="bg-blue-100 relative">
+            <div className="flex">
+              <Image
+                src={"/Icons/anchor-icon.svg"}
+                alt={""}
+                width={30}
+                height={30}
+                className="h1-icon"
+              />
+              <h1 className="heading-1">HSS Trygga möten policy</h1>
+            </div>
+            <p className="paragraph-light">
+              Scouterna centralt initierade arbetet med Trygga Möten 2005 för
+              att stärka ledare i att agera när barn och unga far illa. Målet är
+              att ha en bred kunskap om potentiella risker för att förebygga
+              diskriminering och övergrepp. Hässelby Strands Sjöscoutkår arbetar
+              fortsatt aktivt för att alla medlemmar ska känna sig trygga i
+              verksamheten, och styrelsen har därför antagit denna policy den 12
+              oktober 2025.
+            </p>
+            <div className="text-end">
+              <Button
+                ariaLabel={"read pdf"}
+                href={
+                  "/pdf/Bilaga 20251012 HSS Trygga möten policy Beslutad.pdf"
+                }
+                variant={"primaryBlue"}
+                text={"Läs hela  PDF"}
+              />
+            </div>
             <Image
-              src={"/Icons/anchor-icon.svg"}
+              src={"/Icons/caret_down.svg"}
               alt={""}
               width={30}
               height={30}
-              className="h1-icon"
-            />
-            <h1 className="heading-1">HSS Trygga möten policy</h1>
-          </div>
-          <p className="paragraph-light">
-            Scouterna centralt initierade arbetet med Trygga Möten 2005 för att
-            stärka ledare i att agera när barn och unga far illa. Målet är att
-            ha en bred kunskap om potentiella risker för att förebygga
-            diskriminering och övergrepp. Hässelby Strands Sjöscoutkår arbetar
-            fortsatt aktivt för att alla medlemmar ska känna sig trygga i
-            verksamheten, och styrelsen har därför antagit denna policy den 12
-            oktober 2025.
-          </p>
-          <div className="text-end">
-            <Button
-              ariaLabel={"read pdf"}
-              href={""}
-              variant={"primaryBlue"}
-              text={"Läs hela  PDF"}
+              className="mx-auto"
             />
           </div>
-          <Image
-            src={"/Icons/caret_down.svg"}
-            alt={""}
-            width={30}
-            height={30}
-            className="mx-auto"
-          />
           <div className="flex flex-col md:flex-row">
             <div>
               <div className="flex">
@@ -75,18 +90,18 @@ const PolicyPage = () => {
                 alt={""}
                 width={300}
                 height={300}
-                className="rounded-sm w-full h-auto"
+                className="rounded-sm w-full h-auto mb-6"
               />
               <Button
                 ariaLabel={"read pdf"}
-                href={""}
+                href={"/pdf/HSS-Flytvastpolicy-2025-Beslutad.pdf"}
                 variant={"primaryBlue"}
                 text={"Läs hela  PDF"}
               />
             </div>
           </div>
 
-          <div>
+          <div className="">
             <div className="flex">
               <Image
                 src={"/Icons/camera-icon.svg"}
@@ -103,16 +118,16 @@ const PolicyPage = () => {
               verksamhet. Vi publicerar aldrig namn på våra scouter. Genom att
               vara medlem i HSS samtycker man till detta. Om ditt barn av någon
               anledning inte får vara med på bild så kontakta ledarna så att de
-              får kännedom om det.
-            </p>
-            <p className="paragraph-light">
+              får kännedom om det.<br></br>
               Om ditt barn av någon anledning inte får vara med på bild så
               kontakta ledarna så att de får kännedom om det.
             </p>
+        
+              
             <div className="text-end">
               <Button
-                ariaLabel={"contact"}
-                href={"mailto:info"}
+                ariaLabel={"mail button"}
+                href={"mailto:info@hss-scout.org"}
                 variant={"primaryBlue"}
                 text={"Kontakta oss"}
               />
@@ -125,7 +140,7 @@ const PolicyPage = () => {
               className="mx-auto"
             />
           </div>
-          <div>
+          <div className="">
             <div className="flex">
               <Image
                 src={"/Icons/document-icon.svg"}
@@ -149,25 +164,36 @@ const PolicyPage = () => {
             </p>
           </div>
           <div className="bg-blue-100 border card border-blue-500 rounded-sm">
-            <h4 className="heading-4">Uppgifter vi registrerar</h4>
-            <ul>
+            <h4 className="heading-4 mb-3">Uppgifter vi registrerar</h4>
+            <ul className="pl-5">
               {registerInfo.map((info) => {
                 return (
-                  <li key={info.id} className="list-disc list-decimal">
+                  <li key={info.id} className="list-disc">
                     <p className="paragraph-light">{info.infoContent}</p>
                   </li>
                 );
               })}
             </ul>
-            <div className="text-end">
+            <div className="flex">
+              <Image src={"/Icons/infoblue.png"} alt={""} width={20} height={20} className="w-8 h-8 mr-2 mt-0.5"/>
+              <p className="paragraph-light">
+                Uppgifter om syskon och föräldrar används för att
+                erbjuda syskonförtur och prioritera barn vars föräldrar har en
+                roll i kåren. Vi skriver aldrig in hälsouppgifter i Scoutnet.
+              </p>
+            </div>
+            <div className="text-end" onClick={handelGdpr}>
               <Button
                 ariaLabel={"contact"}
                 href={"mailto:info"}
                 variant={"primaryBlue"}
                 text={"Läs hela GDPR"}
+                
               />
             </div>
           </div>
+          {gdpr && <GDPR handelGdpr={handelGdpr}/>}
+         
           <div className="card bg-yellow-100 border border-accent">
             <p className="paragraph-light">
               🔒 Endast ledare och styrelse inom HSS har tillgång till
@@ -194,10 +220,14 @@ const PolicyPage = () => {
             </p>
           </div>
           <div className="bg-blue-100 card border border-blue-500">
-            <h3 className="heading-3">Uppgifter som samlas in</h3>
-            <ul>
+            <h3 className="heading-4 mb-3">Uppgifter som samlas in</h3>
+            <ul className="pl-5">
               {collectedInformation.map((item) => {
-                return <li className="list-disc"><p className="paragraph-light">{item.information}</p></li>;
+                return (
+                  <li className="list-disc" key={item.id}>
+                    <p className="paragraph-light">{item.information}</p>
+                  </li>
+                );
               })}
             </ul>
           </div>
---
+++ src/components/gdpr/index.tsx
@@ -0,0 +1,51 @@
+import Button from "../buttons/Button";
+
+export default function GDPR({handelGdpr}:{handelGdpr:() => void}) {
+
+
+ 
+  return (
+    <div className="bg-background fixed inset-0 w-screen mx-auto overflow-y-auto z-50">
+      <div className="container">
+       <p className="paragraph-light ">
+        Vi är måna om att du ska känna dig trygg i dina kontakter med oss.
+        Därför vill vi berätta om vår hantering av personuppgifter och om den
+        nya dataskyddsförordningen (GDPR) som gäller i hela EU från och med den
+        25 maj 2018.<br></br>
+        Scouterna (802006-2942) är tillsammans med din lokala scoutkår gemensamt
+        personuppgiftsansvarig för de uppgifter du lämnar till oss.<br></br>
+        De personuppgifter vi samlar in och behandlar är framför allt sådana som
+        du själv uppger till oss (som personnummer, namn, adress, e-postadress,
+        telefonnummer) när du ansöker om medlemskap. Vi behöver dessa uppgifter
+        för att hantera din medlemsansökan och administrera ditt medlemskap hos
+        oss. Dessutom skickar vi dig medlemstidning, nyhetsbrev, erbjudandet
+        från scoutshopen, erbjudanden om deltaganden i olika aktiviteter som
+        t.ex. läger, kurser och arrangemang. Dina uppgifter behöver behandlas av
+        oss för att vi ska kunna fullgöra våra skyldigheter gentemot dig som
+        medlem.<br></br>
+        Vi delar inte dina personuppgifter med andra, utom när vi har ett avtal
+        med en tredje part. Det kan vara företag som förmedlar betalningar,
+        trycker och skickar vår medlemstidning, beviljar bidrag till oss, eller
+        på annat sätt är viktiga för vår verksamhet.<br></br>
+        Du har rätt att begära tillgång till, rättelse eller under vissa
+        förutsättningar radering av personuppgifter eller begränsning av
+        behandling som rör dig. Tänk då på att det kan innebära att vi inte kan
+        fullgöra våra förpliktelser gentemot dig som medlem. Du har vidare rätt
+        att invända mot behandlingen. Som medlem med en registrerad
+        e-postadress, kan du skapa en inloggning och logga in i medlemssystemet
+        Scoutnet och kontrollera att vi har rätt uppgifter om dig. Då kan du
+        också korrigera dina uppgifter om någonting ändrats, t.ex. om byter
+        adress m.m.<br></br>
+        Om du har frågor om vår behandling av personuppgifter är du välkommen
+        att kontakta oss på scoutnet@scouterna.se<br></br>
+        Utöver detta kan din lokala scoutkår behöva behandla dina
+        personuppgifter för andra ändamål som i så fall specificeras här nedan.
+        Kårspecifik information kring personuppgiftshantering
+      </p>
+       <div className="flex justify-end ">
+        <button className="bg-primary text-accent px-6 py-3 rounded-sm " onClick={handelGdpr}>Stäng</button>
+       </div>
+      </div>
+    </div>
+  );
+}
