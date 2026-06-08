6dabe8768fa283fa1ea551cbbebde9e325cce5a4
Clara Swahn <clara.swahn@gmail.com>
2026-06-08 22:53:40 +0200

    fix(071): change background wave

--- src/app/[locale]/(for-medlemmar)/aret-i-hss/page.tsx
+++ src/app/[locale]/(for-medlemmar)/aret-i-hss/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 const AretiHssPage = () => {
   return (
     <main id="main-content">
-      <section className="full-bleed flex flex-col lg:bg-[url(/images/Backgrounds/blueWavyBoat5.png)] lg:bg-size-[100%] lg:bg-no-repeat lg:bg-top bg-[url(/images/Backgrounds/boatBackground1.png)] bg-size-[175px] bg-no-repeat bg-right">
+      <section className="full-bleed flex flex-col lg:bg-[url(/images/Backgrounds/blueWavyBoat2.png)] lg:bg-size-[100%] lg:bg-no-repeat lg:bg-top bg-[url(/images/Backgrounds/boatBackground1.png)] bg-size-[175px] bg-no-repeat bg-right">
         <div className="container flex justify-center lg:justify-start flex-col items-center lg:flex-row">
           <div className="flex lg:flex-row">
             <Image className="h2-icon" src={`/Icons/yellow-calendar-icon.svg`} height={30} width={30} alt="" />
