30e5fae4eb7ec78daccf00640913c4f1d633d983
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-06-08 12:09:27 +0200

    fix(077): types second try fix

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,70 @@
+export type ageGroupCardContentType = {
+  title: string,
+  ageGroup: string,
+  ageRange: string,
+  subtitle: string,
+  info: string,
+  meetings: string,
+  colourSchemeBorder: string,
+  colourSchemePrimary: string,
+  colourSchemeSecondary: string,
+  icon: string,
+  image: string,
+  price: string,
+  apply: string
+}
+
+export type markesaffischCardContentType = {
+  title: string,
+  age: string,
+  subtitle: string,
+  mobileImage: string[],
+  desktopImage: string,
+  colourSchemeBorder: string,
+  colourSchemePrimary: string,
+  colourSchemeSecondary: string
+  colourSchemeHeading: string,
+  icon: string,
+}
+
+export type scoutUniformCardContentType = {
+  title: string,
+  subtitle: string,
+  image_mobile_subtitle: string,
+  second_subtitle: string,
+  image_mobile_second_subtitle: string,
+  third_subtitle: string,
+  image_mobile_third_subtitle: string,
+  desktopImage: string,
+}
+
+export type bliLedareCardContentType = {
+  title: string,
+  subtitle: string,
+  icon: string,
+}
+
+export type engageraDigCardContentType = {
+  title: string,
+  icon: string,
+}
+
+export type MenuItemsType = {
+  titleKey: string;
+  content:
+  {
+    textKey: string;
+    href: string;
+    icon: string;
+    mobileIcon: string
+  }[];
+}
+
+export type pageHeaderImageType = {
+  path: string,
+  image: string,
+}
+
 export type SecurityAndSafetyCardContentType = {
   title: string,
   icon: string,
