f5365ec2ac64dcb3495ecc6944e0c96243387a22
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-06-08 11:59:29 +0200

    types.ts added

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,117 +1,53 @@
-export type ageGroupCardContentType = {
-  title: string,
-  ageGroup: string,
-  ageRange: string,
-  subtitle: string,
-  info: string,
-  meetings: string,
-  colourSchemeBorder: string,
-  colourSchemePrimary: string,
-  colourSchemeSecondary: string,
-  icon: string,
-  image: string,
-  price: string,
-  apply: string
-}
-
-export type markesaffischCardContentType = {
-  title: string,
-  age: string,
-  subtitle: string,
-  mobileImage: string[],
-  desktopImage: string,
-  colourSchemeBorder: string,
-  colourSchemePrimary: string,
-  colourSchemeSecondary: string
-  colourSchemeHeading: string,
-  icon: string,
-}
-
-export type scoutUniformCardContentType = {
-  title: string,
-  subtitle: string,
-  image_mobile_subtitle: string,
-  second_subtitle: string,
-  image_mobile_second_subtitle: string,
-  third_subtitle: string,
-  image_mobile_third_subtitle: string,
-  desktopImage: string,
-}
-
-export type bliLedareCardContentType = {
-  title: string,
-  subtitle: string,
-  icon: string,
-}
-
-export type engageraDigCardContentType = {
-  title: string,
-  icon: string,
-}
-
-export type MenuItemsType = {
-  titleKey: string;
-  content:
-  {
-    textKey: string;
-    href: string;
-    icon: string;
-    mobileIcon: string
-  }[];
-}
-
-export type pageHeaderImageType = {
-  path: string,
-  image: string,
-}
-
 export type SecurityAndSafetyCardContentType = {
   title: string,
   icon: string,
   info: string
-}
-
-export type SecurityAndSafetyCheckType = {
+  }
+  
+  export type SecurityAndSafetyCheckType = {
   info: string,
   icon: string
-}
-
-export type SecurityAndSafetyInfoType = {
+  }
+  
+  export type SecurityAndSafetyInfoType = {
   title: string,
   info: string
-}
-
-export type ActivityCardType = {
+  }
+  
+  export type ActivityCardType = {
   icon: string,
   title: string,
   info: string
-}
-
-export type ActivityFeatureType = {
-  titleKey: string,
-  subTitleKey: string,
-  subIcon: string,
-  infoKey: string,
-  infoDescFirstKey: string,
-  infoDescSecondKey: string,
-  icon: string
-}
-
-export type vilkaArViContentType = {
-  image: string,
-}
-
-export type vilkaArViGamlaBatarContentType = {
-  image: string,
-}
-
-export type vilkaArViTextContentType = {
+  }
+  
+  export type ActivityFeatureType = {
+      titleKey: string,
+      subTitleKey: string,
+      subIcon: string,
+      infoKey: string,
+      infoDescFirstKey: string,
+      infoDescSecondKey: string,
+      icon: string
+  }
+  
+  export type FaqProps = {
+    id:number,
+    title:string,
+    content:string
+  }
+  
+  export type VaraBatarContentType = {
+    image: string,
+    titleKey: string,
+    subTitleKey: string,
+    subIcon: string,
+    infoKey: string,
+    infoDescFirstKey: string,
+    infoDescSecondKey: string,
+    icon: string
+  }
+  
+  export type vilkaArViContentType = {
   image: string,
   info: string
-}
-
-export type FaqProps = {
-  id:number,
-  title:string,
-  content:string
-}
+  }
