239a38224eb39d9be485692f9355c54028c9c401
SgnCycles <signe.bekere@gmail.com>
2026-06-09 15:16:32 +0200

    fix: agegroup icon sizing

--- src/components/cards/AgeGroupCardHome/index.tsx
+++ src/components/cards/AgeGroupCardHome/index.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { ageGroupCardContent } from "@/data/ageGroupCardContent";
 import Image from "next/image";
-import useIsMobile from "@/hooks/useGetMobile";
+import Button from "@/components/buttons/Button";
 
 const AgeGroupCardHome = () => {
-  const isMobile = useIsMobile();
 
   return (
-    <article className="grid place-items-center md:pt-6 pb-6">
-      <div className="w-[90%] sm:w-[90%] lg:w-[70%] xl:w-[60%]">
+    <section className="bg-yellow-100 grid place-items-center">
+      <article className="container">
         <div className="flex justify-center sm:justify-start pt-6 pb-4">
           <Image
             src={`/Icons/anchor-icon.svg`}
@@ -17,36 +16,45 @@ const AgeGroupCardHome = () => {
             alt=""
             className="pb-2"
           />
-          <h2 className="text-blue-900 font-heading text-[30px] md:text-[40px] font-extrabold pl-2 md:text-5xl">
+          <h2 className="heading-2">
             Åldersgrupper
           </h2>
         </div>
         <div className="sm:flex pb-6 sm:justify-between">
-          <p className="sm:w-[70%] text-base text-center sm:text-left md:text-xl">
+          <p className="paragraph-light text-center sm:text-left">
             Oavsett ålder finns det en plats för dig i HSS.
             <br />
             Varje avdelning har sin egen karaktär och sina egna äventyr.
           </p>
+          <div className="hidden place-content-center md:flex flex-wrap text-xl">
+            <Button
+              ariaLabel=""
+              href="/aldersgrupper"
+              variant="primaryBlue"
+              text="Läs mer här"
+            />
+          </div>
         </div>
         <ul className="flex flex-col h-auto justify-center items-center gap-6 pb-6">
           {ageGroupCardContent &&
             ageGroupCardContent.map((card, index) => (
               <li
                 key={index}
-                className={`flex flex-col justify-between w-full h-full md:min-h-40 border ${card.colourSchemeBorder} pt-2 md:pt-4 rounded-tl-sm rounded-tr-sm rounded-bl-xs rounded-br-xs overflow-hidden`}
+                className={`bg-background flex flex-col justify-between w-full h-full md:min-h-30 border ${card.colourSchemeBorder} pt-2 md:pt-4 rounded-tl-sm rounded-tr-sm rounded-bl-xs rounded-br-xs overflow-hidden`}
               >
-                <article className="flex">
+                <div className="flex md:pt-5 md:pb-5 md:pr-5">
                   <div className="flex justify-center items-center w-[30%]">
                     <Image
                       src={`/images/Agegroup/${card.icon}`}
-                      height={isMobile ? 50 : 90}
-                      width={isMobile ? 50 : 90}
+                      height={60}
+                      width={60}
                       alt={`${card.title} emblem`}
+                      className="w-15 h-auto md:w-22.5"
                     ></Image>
                   </div>
                   <div className="flex flex-col w-full items-start pb-4 md:pb-0">
                     <div className="flex flex-col md:flex-row">
-                      <h3 className="font-semibold font-heading text-lg md:text-2xl text-blue-900 tracking-wide pb-2 pl-4 md:pb-0 md:pl-0">
+                      <h3 className="font-semibold font-heading text-[24px] text-blue-900 tracking-wide pb-2 pl-4 md:pb-0 md:pl-0">
                         {card.title}
                       </h3>
                       <p
@@ -58,25 +66,31 @@ const AgeGroupCardHome = () => {
                         </span>
                       </p>
                     </div>
-                    <p className="sr-only md:not-sr-only font-body pr-4 text-md md:pt-2">
+                    <p className="sr-only md:not-sr-only paragraph-light">
                       {card.subtitle}
                     </p>
                   </div>
-                </article>
+                </div>
                 <div
                   className={`flex justify-center sm:justify-between ${card.colourSchemePrimary} font-body text-white font-bold`}
                 >
-                  <p
-                    className={`place-content-center text-left pl-4 h-8 text-sm`}
-                  >
+                  <p className={`place-content-center text-left pl-4 h-8`}>
                     {card.meetings}
                   </p>
                 </div>
               </li>
             ))}
         </ul>
-      </div>
-    </article>
+        <div className="flex sm:hidden items-center justify-center text-xl">
+          <Button
+            ariaLabel=""
+            href="/aldersgrupper"
+            variant="secondaryBlue"
+            text="Läs mer om åldersgrupper"
+          />
+        </div>
+      </article>
+    </section>
   );
 };
 
