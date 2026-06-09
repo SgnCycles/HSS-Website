8f2f2f1c981b59e16b4de9d5cd2201ab270dfabd
Clara Swahn <clara.swahn@gmail.com>
2026-06-09 19:42:27 +0200

    fix(071): fix ul to div

--- src/app/[locale]/(for-medlemmar)/aret-i-hss/page.tsx
+++ src/app/[locale]/(for-medlemmar)/aret-i-hss/page.tsx
@@ -7,10 +7,10 @@ const AretiHssPage = () => {
     <main id="main-content" className="bg-background bg-contain bg-no-repeat lg:bg-[url(/images/Backgrounds/blueWavyBoat2.png)] lg:bg-size-[100%] lg:bg-no-repeat lg:bg-top">
       <section className="container section flex flex-col bg-[url(/images/Backgrounds/boatBackground1.png)] bg-size-[175px] bg-no-repeat bg-right lg:bg-none">
         <div className="flex justify-center lg:justify-start flex-col items-center lg:flex-row">
-          <ul className="flex flex-row">
+          <div className="flex flex-row">
             <Image className="h2-icon" src={`/Icons/yellow-calendar-icon.svg`} height={30} width={30} alt="" />
             <h1 className="heading-2">Året i HSS</h1>
-          </ul>
+          </div>
           <div className="text-base font-body flex gap-2 pl-2 pr-2 justify-center rounded-md w-fit border bg-secondary border-accent text-center self-center lg:ml-4">
             <p>Återkommande aktiviteter och traditioner</p>
           </div>
