2fcd9dc228a4174289ff80cad891a3432fbbb27d
Clara Swahn <clara.swahn@gmail.com>
2026-06-09 19:34:58 +0200

    fix(071): fix semantics and styling

--- src/components/cards/AretIHssCard/index.tsx
+++ src/components/cards/AretIHssCard/index.tsx
@@ -6,25 +6,27 @@ const AretiHssCard = (content: aretIHssContentType) => {
   const { title, icon, months } = content
 
   return (
-    <article className="container flex flex-col gap-3.75">
-      <div className="border-b-5 border-accent flex w-fit gap-1 items-center pb-1">
-        <h3 className="heading-3 text-blue-500">{title}</h3>
-        <Image className="h2-icon" src={`/Icons/${icon}`} height={25} width={25} alt="" />
+    <li className="grid grid-cols-1 md:grid-cols-[1fr_5fr] auto-rows-auto gap-4 md:gap-y-6 md:gap-x-12">
+      <div className="md:col-start-1 md:row-start-1 row-span-full">
+        <div className="border-b-5 border-accent flex w-fit md:w-full gap-1 items-center pb-1">
+          <h2 className="heading-3 text-blue-500">{title}</h2>
+          <Image className="h2-icon mr-0" src={`/Icons/${icon}`} height={25} width={25} alt="" />
+        </div>
       </div>
       {months && months.map((month, index) => {
         const information = month.info.split('|')
         return (
-          <div key={index}>
-            <p className="paragraph-light font-bold text-blue-900 mb-0">{month.subtitle}</p>
+          <section className="md:col-start-2" key={index}>
+            <h3 className="paragraph-light font-bold text-blue-900 mb-0">{month.subtitle}</h3>
             {information && information.map((info, index) => {
               return (
                 <p key={index} className="paragraph-light mb-0">{info}</p>
               )
             })}
-          </div>
+          </section>
         )
       })}
-    </article >
+    </li >
   )
 }
 
