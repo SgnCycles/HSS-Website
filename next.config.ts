3e06d5c16e06d903e7c019d17fac55401dd5ef73
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-06-10 15:55:06 +0200

    feat(093)-initialize branch from main and bring changes from feat/061-upptack-livet-som-sjoscout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent-ord5-2.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn2.behold.pictures",
+      },
+    ],
+  },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
