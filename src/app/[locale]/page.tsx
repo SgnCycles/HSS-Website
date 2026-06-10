a4b45adb3f397123ef85f503c3e9ad743a5d04e4
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-06-10 13:36:50 +0200

    feat(061): add component to page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,13 @@ import BliLedareHome from "@/components/cards/BliLedare";
 import ActivityFeature from "@/components/cards/ActivityFeature";
 import BorderTwoDown from "@/components/borders/BorderTwoDown";
 import BorderSingleUp from "@/components/borders/BorderSingleUp";
+import UpptäckLivetSomSjöscout from "@/components/UpptäcklivetSomSjöscout";
 
 export default function Home() {
   return (
     <main id="main-content">
       <ActivityFeature />
+      <UpptäckLivetSomSjöscout />
       <BorderSingleUp variant="text-yellow-100" />
       <AgeGroupCardHome />
       <BorderSingleUp variant="text-primary bg-yellow-100" />
