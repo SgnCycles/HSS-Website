0d3db2602640a63ef8f07ff4ff06877e086e5aa3
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-06-10 11:51:23 +0200

    feat(061): add live Instagram feed component

--- src/components/UpptäcklivetSomSjöscout/index.tsx
+++ src/components/UpptäcklivetSomSjöscout/index.tsx
@@ -4,75 +4,70 @@ import Link from "next/link";
 
 export default function UpptäckLivetSomSjöscout() {
   return (
-    <section className="w-full bg-blue-100">
-      <div className="container">
-        <Image
-          src="/Icons/fe_arrow-down.svg"
-          alt=""
-          width={59}
-          height={59}
-          className="hidden md:block top-10 mx-auto"
-        />
-        <div className="flex flex-col items-start w-full md:max-w-6xl mx-auto gap-6">
-          <h2 className="flex heading-2 items-center justify-between">
-            <Image
-              src="/Icons/yellow-instagram-icon.svg"
-              alt=""
-              width={41}
-              height={41}
-              className="h2-icon"
-            />
-            <span className="heading-3 font-bold md:heading-1">
-              Upptäck livet som sjöscout
-            </span>
-          </h2>
-
-          <div className="flex flex-col md:flex-row items-center gap-6 w-full order-1 md:order-2">
-            <p className="paragraph flex-1 order-1 md:order-2">
-              Kliv in i vår vardag, läger, segling, knutar och kamratskap. Se
-              våra äventyr, vänskap och minnen som vi skapar tillsammans.
-            </p>
-
-            <Link
-              href="https://www.instagram.com/hss_scout/"
-              rel="noopener noreferrer"
-              className="flex w-fit gap-3 justify-center items-center bg-primary px-6 py-3 rounded-sm order-3 md:order-1"
-            >
+    <main id="main-content">
+      <section className=" bg-blue-100">
+        <div className="container">
+          <Image
+            src="/Icons/fe_arrow-down.svg"
+            alt=""
+            width={59}
+            height={59}
+            className="hidden md:block top-10 mx-auto"
+          />
+          <div className="flex flex-col items-start w-full md:max-w-6xl mx-auto gap-6">
+            <h2 className="flex heading-2 items-center justify-between">
               <Image
-                src="/Icons/skill-icons_instagram.svg"
+                src="/Icons/yellow-instagram-icon.svg"
                 alt=""
-                width={40}
-                height={40}
-                className="w-10 h-10"
+                width={41}
+                height={41}
+                className="h2-icon"
               />
-              <div className="flex flex-col ">
-                <h3 className="heading-3 text-accent whitespace-nowrap">
-                  Följ oss på instagram
+              <span className="heading-3 font-bold md:heading-1">
+                Upptäck livet som sjöscout
+              </span>
+            </h2>
+
+            <div className="flex flex-col md:flex-row items-center gap-6 w-full order-1 md:order-2">
+              <p className="paragraph flex-1 order-1 md:order-2">
+                Kliv in i vår vardag, läger, segling, knutar och kamratskap. Se
+                våra äventyr, vänskap och minnen som vi skapar tillsammans.{" "}
+                <span className="paragraph text-accent font-extrabold flex-1">
+                  @hss_scout
+                </span>
+              </p>
+
+              <Link
+                href="https://www.instagram.com/hss_scout/"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center w-full md:w-60 p-2 bg-primary rounded-sm order-3 md:order-1"
+              >
+                <Image
+                  src="/Icons/skill-icons_instagram.svg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className="w-10 h-10 p-2 md:p-0"
+                />
+                <h3 className="heading-3 text-accent">
+                  Följ oss<span className="pl-1">&#8594;</span>
                 </h3>
-                <p className="paragraph text-background flex-1">@hss_scout</p>
-              </div>
-              <Image
-                src="/Icons/yellowArrow.svg"
-                alt=""
-                width={23}
-                height={23}
-                className="transition-transform w-8 h-8 animate-pulse duration-100"
-              />
-            </Link>
-          </div>
+              </Link>
+            </div>
 
-          <div className="w-full order-2 md:order-3">
-            <InstaFeedCard />
+            <div className="w-full order-2 md:order-3">
+              <InstaFeedCard />
+            </div>
           </div>
+          <Image
+            src="/Icons/fe_arrow-down.svg"
+            alt=""
+            width={59}
+            height={59}
+            className="hidden md:block bottom-0 mx-auto"
+          />
         </div>
-        <Image
-          src="/Icons/fe_arrow-down.svg"
-          alt=""
-          width={59}
-          height={59}
-          className="hidden md:block bottom-0 mx-auto"
-        />
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
--- src/components/cards/InstaFeedCard/index.tsx
+++ src/components/cards/InstaFeedCard/index.tsx
@@ -1,24 +1,86 @@
+"use client";
 import Image from "next/image";
-import { instaFeed } from "@/data/upptäckLivetSomSjöscout";
+import { useEffect, useState } from "react";
+import { MessageCircle } from "lucide-react";
+import Link from "next/link";
+
+interface FeedProps {
+  id?: string;
+  caption?: string;
+  mediaUrl: string;
+  permalink: string;
+  likeCount: number;
+  commentsCount: number;
+  username: string;
+}
 
 export default function InstaFeedCard() {
+  const [apiData, setApiData] = useState<FeedProps[]>([]);
+  const [userName, setUserName] = useState("");
+
+  const url = "https://feeds.behold.so/au47cEddhn57lA49VlAe";
+
+  const fetchInstaData = async () => {
+    try {
+      const res = await fetch(url, { next: { revalidate: 3600 } });
+
+      if (!res.ok) {
+        console.log(`Error Occured ${res.status}`);
+      }
+
+      const data = await res.json();
+      setUserName(data.username);
+      setApiData(data.posts);
+    } catch (error) {
+      console.log("An Error Occured", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchInstaData();
+  }, []);
+
   return (
-    <div className="flex md:gap-4 gap-2">
-      {instaFeed.map((post) => {
-        return (
-          <div
-            key={post.id}
-            className="relative flex-1 min-w-0 transition-transform hover:scale-95 duration-100 cursor-pointer">
-            <Image
-              src={post.img}
-              alt=""
-              width={432}
-              height={560}
-              className="object-cover w-full h-27 md:h-125"
-            />
-          </div>
-        );
-      })}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  container items-center w-full">
+      {apiData &&
+        apiData?.slice(0, 3).map((post) => {
+          return (
+            <div
+              key={post.id}
+              className=" bg-background rounded-md overflow-hidden shadow-md transition hover:-translate-y-1 hover:shadow-xl"
+            >
+              <Link
+                className="shadow-2xl  shadow-black rounded-sm"
+                href={post.permalink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={post.mediaUrl}
+                  alt=""
+                  width={300}
+                  height={400}
+                  className="w-full h-auto"
+                />
+                <div className="flex justify-start items-center">
+                  <p className="p-2.5">
+                    Likes <span>{post.likeCount}</span>
+                  </p>
+                  <MessageCircle className="" />{" "}
+                  <span className="pl-1.5">{post.commentsCount}</span>
+                </div>
+              </Link>
+              <div className="mt-4 px-2 ">
+                <p className="text-sm line-clamp-2">
+                  <span className="font-bold">{userName}</span> {post.caption}
+                </p>
+                <button className="mt-2 text-sm text-grey-500 font-medium pb-3.5">
+                  See translation
+                </button>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
