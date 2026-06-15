610f91f805578adfa8b3e985a255969ff65f5870
Emelie B. Silfverberg <emelieb.silfverberg@Emelies-MacBook-Air.local>
2026-06-15 14:19:11 +0200

    feat(115): mobile images fix

--- src/components/cards/VilkaArViMobile/index.tsx
+++ src/components/cards/VilkaArViMobile/index.tsx
@@ -9,7 +9,7 @@ const VilkaArViCardMobile = () => {
       {vilkaArVi && vilkaArVi.map((card) =>
         <div key={card.id} className="flex flex-col items-center w-screen">
           <p className="container paragraph-light mb-0">{t(`about-us.${card.id}.text`)}</p>
-          <Image src={`/images/Body/${card.image}`} height={276} width={393} alt="vilka är vi" className="w-[-webkit-fill-available]" />
+          <Image src={card.image} height={276} width={393} alt="vilka är vi" className="w-[-webkit-fill-available]" />
         </div>
       )}
     </article>
