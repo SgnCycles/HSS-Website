e92b238d3f1d172a9d1be7dd5f9a09563a5c9ef4
Clara Swahn <clara.swahn@gmail.com>
2026-06-15 15:03:47 +0200

    fix(084): fix title on bli ledare

--- src/components/cards/BliLedareMainSection/index.tsx
+++ src/components/cards/BliLedareMainSection/index.tsx
@@ -22,16 +22,17 @@ const BliLedareMainSection = () => {
         <article className="container">
           <div className="flex w-full justify-center lg:justify-start items-center pb-6">
             <Image src={`/Icons/bliledare_blue.svg`} height={30} width={30} alt="" className="h1-icon" />
-            <h1 className="heading-1">{t('heading')}</h1>
+            <h1 className="heading-1">{t("heading1")}<span className="hidden sm:inline-block">{t("heading2")}</span>
+            </h1>
           </div>
-          <p className="paragraph-light text-grey-500">{t('intro')}</p>
+          <p className="paragraph-light text-grey-500">{t("intro")}</p>
           <div className="flex flex-col justify-between lg:flex-row">
             <BliLedareBenefitCard />
             <BliLedareContentCardMobile />
             <div className="hidden lg:flex flex-col justify-between items-center lg:w-auto">
               <Image className="lg:rounded-md w-full lg:w-75 h-auto" src="/images/Body/bliLedare_body1.png" height={600} width={600} alt="" />
-              <Button ariaLabel="" href="https://www.scoutnet.se/register/in/group/764" variant="primaryBlue" text={t('cta')} target="_blank" />
-              <ReadMoreButton ariaLabel="Open to read more" variant="secondaryBlue" text={t('readMore')} onClick={handleClick} />
+              <Button ariaLabel="" href="https://www.scoutnet.se/register/in/group/764" variant="primaryBlue" text={t("cta")} target="_blank" />
+              <ReadMoreButton ariaLabel="Open to read more" variant="secondaryBlue" text={t("readMore")} onClick={handleClick} />
             </div>
           </div>
           <p className="text-base font-bold text-center text-primary text-extrabold lg:hidden">{t('details.p6')}</p>
@@ -41,7 +42,7 @@ const BliLedareMainSection = () => {
         <article className="container text-primary">
           <div className="flex w-full justify-center md:justify-start items-center pb-6">
             <Image src="/Icons/bliledare_blue.svg" height={30} width={30} alt="" className="h1-icon" />
-            <h1 className="heading-1">{t('heading')}</h1>
+            <h1 className="heading-1">{t("heading")}</h1>
           </div>
           <div className="flex flex-col paragraph-light mb-6">
             <BliLedareContentCardDesktop />
@@ -50,7 +51,7 @@ const BliLedareMainSection = () => {
                 className="flex bg-primary font-semibold text-accent text-center px-16 transition-all duration-300 border rounded-sm py-2 cursor-pointer hover:bg-primary-hover hover:text-background"
                 onClick={handleClick}
               >
-                <p>{t('closeCta')}</p>
+                <p>{t("closeCta")}</p>
               </button>
             </div>
           </div>
